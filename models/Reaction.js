5a6f05a880277762b46affee380b3021b8c364e4
Gustavo Martinez <gmtz.0794@gmail.com>
2023-11-29 15:37:03 -0800

    modified models

---
+++ models/Reaction.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+  reactionBody: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Reaction = model('Reaction', reactionSchema);
+
+module.exports = Reaction;
