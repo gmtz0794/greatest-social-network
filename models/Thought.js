fc569c7d2d4f595f95f9cb8666d35d52bac6a7a5
Gustavo Martinez <gmtz.0794@gmail.com>
2024-11-25 19:24:19 -0800

    rework the application

--- models/Thought.js
+++ models/Thought.js
@@ -1,26 +1,36 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Reaction',
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'You need to leave a thought!',
+      minlength: 1,
+      maxlength: 280
     },
-  ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
 });
 
 const Thought = model('Thought', thoughtSchema);
--- routes/api/friend-Routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const friendController = require('../../controllers/friendController');
-
-router.post('/:userId', friendController.addFriend);
-
-router.delete('/:userId', friendController.removeFriend);
-
-module.exports = router;
